43e52ce9db9ac74f69685bcb4459b5a1b7b81855
Marie Katrine Ekeberg <mke@themkat.net>
2022-05-18 17:35:22 +0200

    Made processor types configurable

--- lsp-m68k.el
+++ lsp-m68k.el
@@ -17,11 +17,25 @@
  '(:npm :package "m68k-lsp-server"
         :path "m68k-lsp-server"))
 
+;; TODO: how many of the configuration options should we add? All? 
+(defcustom m68k-processor-types ["mc68000"]
+  "Types of processors we are targeting (e.g, mc68000, mc68020, mc68881 etc.). Set to get fitting completion candidates, documentation etc."
+  :group 'm68k
+  :type 'vector)
+
+(lsp-register-custom-settings
+ '(("m68k.format.case" "lower")
+   ("m68k.processors" m68k-processor-types)))
+
+
 (lsp-register-client
  (make-lsp-client
-  :new-connection (lsp-stdio-connection)
+  :new-connection (lsp-stdio-connection (lambda () (list "m68k-lsp-server" "--stdio")))
   :priority -1
   :activation-fn (lsp-activate-on "m68k")
+  :initialized-fn (lambda (workspace)
+                    (with-lsp-workspace workspace
+                      (lsp--set-configuration (lsp-configuration-section "m68k"))))
   :server-id 'm68k-ls
   :download-server-fn (lambda (_client callback error-callback _update?)
                         (lsp-package-ensure 'm68k-lsp-server callback error-callback))))
