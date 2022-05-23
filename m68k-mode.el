eedaad8f928878c5ff22b7e432e7ebdc8835f248
Marie Katrine Ekeberg <mke@themkat.net>
2022-05-23 20:35:01 +0200

    Minor corrections

--- m68k-mode.el
+++ m68k-mode.el
@@ -1,5 +1,14 @@
+;;; m68k-mode.el --- Major mode and utilities for M68k assembly programming
 
-;; Package-Requires: ((emacs "25.1") (lsp-mode "8.0.0") (tree-sitter "0.12.0"))
+;; URL: https://github.com/themkat/emacs-m68k
+
+;; Package-Requires: ((emacs "25.1") (lsp-mode "8.0.0") (dap-mode "0.7") (tree-sitter "0.12.0"))
+;; Version 0.0.1
+
+;;; Commentary:
+;; Various utilities for programming M68k assembly. This includes lsp, debugging and more.
+
+;;; Code:
 
 (define-derived-mode m68k-mode
   asm-mode "m68k-mode"
@@ -10,7 +19,7 @@
 ;; requires https://github.com/grahambates/tree-sitter-m68k
 ;; TODO: setup method for tree-sitter-m68k?
 ;; TODO: any way we can make this optional if grammar not installed?
-(add-to-list 'tree-sitter-major-mode-language-alist '(m68k-mode . m68k))
+;;(add-to-list 'tree-sitter-major-mode-language-alist '(m68k-mode . m68k))
 
 (defcustom m68k-tree-sitter-enabled nil
   "Turns on tree-sitter mode for better syntax highlighting. Requires you to set up tree-sitter-m68k and tree-sitter mode.")
@@ -29,3 +38,4 @@
 ;;(require 'dap-m68k)
 
 (provide 'm68k-mode)
+;;; m68k-mode.el ends here
