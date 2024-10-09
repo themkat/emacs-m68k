e395693d2dab73de4850867040b5ce859863465f
Marie Katrine Ekeberg <mke@themkat.net>
2024-10-09 18:50:03 +0200

    Re-order lsp source for readability

--- lsp-m68k.el
+++ lsp-m68k.el
@@ -15,13 +15,6 @@
 (add-to-list 'm68k-mode-hook #'lsp)
 (add-to-list 'lsp-language-id-configuration '(m68k-mode . "m68k"))
 
-(lsp-dependency
- 'm68k-lsp-server
- '(:system "m68k-lsp-server")
- '(:npm :package "m68k-lsp-server"
-        :path "m68k-lsp-server"))
-
-;; TODO: how many of the configuration options should we add
 (defcustom m68k-processor-types ["mc68000"]
   "Types of processors we are targeting (e.g, mc68000, mc68020, mc68881 etc.). Set to get fitting completion candidates, documentation etc."
   :group 'm68k
@@ -31,6 +24,11 @@
  '(("m68k.format.case" "lower")
    ("m68k.processors" m68k-processor-types)))
 
+(lsp-dependency
+ 'm68k-lsp-server
+ '(:system "m68k-lsp-server")
+ '(:npm :package "m68k-lsp-server"
+        :path "m68k-lsp-server"))
 
 (lsp-register-client
  (make-lsp-client
